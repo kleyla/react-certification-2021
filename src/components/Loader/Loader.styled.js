0f3711d9a0f947f1cd9a22983d4195ef131da9e6
kleyla <kleyla01@live.com>
2021-08-23 13:10:18 -0400

    :sparkle: add Loader component

---
+++ src/components/Loader/Loader.styled.js
@@ -0,0 +1,18 @@
+import styled, { keyframes } from 'styled-components';
+
+const spin = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+export const LoaderCircle = styled.div`
+  border: 8px solid ${(props) => props.theme.headerBg};
+  border-top: 8px solid #ccc;
+  border-radius: 50%;
+  width: 80px;
+  height: 80px;
+  animation: ${spin} 2s linear infinite;
+`;
