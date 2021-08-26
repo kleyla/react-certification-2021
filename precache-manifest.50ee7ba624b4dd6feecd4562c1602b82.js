6228928c3149540ef413fed7bcb6919d61cb3193
kleyla <kleyla01@live.com>
2021-08-25 20:04:44 -0400

    Updates

---
+++ precache-manifest.50ee7ba624b4dd6feecd4562c1602b82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d740d02125417216e201d419a6c4fc17",
+    "url": "/react-certification-2021/index.html"
+  },
+  {
+    "revision": "fbaf61398d0c414e8590",
+    "url": "/react-certification-2021/static/js/2.a017cd1d.chunk.js"
+  },
+  {
+    "revision": "4067c3c9558c190e956627bbe128373a",
+    "url": "/react-certification-2021/static/js/2.a017cd1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aed048e1d67566c08f60",
+    "url": "/react-certification-2021/static/js/main.280e9fa2.chunk.js"
+  },
+  {
+    "revision": "c25ef8b25a78f0d18d30",
+    "url": "/react-certification-2021/static/js/runtime-main.d69dc15b.js"
+  }
+]);
